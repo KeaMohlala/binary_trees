4b1a70ae8f4f6a5460ac38081d1bb18feed8ae04
KeaMohlala <keamogetsemhll@gmail.com>
2024-03-27 10:17:55 -0700

    task 18 add sibling function to the file

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,6 +1,24 @@
 #include "binary_trees.h"
 
 /**
+* binary_tree_sibling - finds the sibling of a node
+* @node: pointer to the node to find the sibling
+* Return: pointer to the sibling node or NULL if not found
+*/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+if (!node || !node->parent)
+return (NULL);
+
+if (node->parent->left == NULL || node->parent->right == NULL)
+return (NULL);
+
+return ((node->parent->left == node) ? node->parent->right :
+node->parent->left);
+}
+
+/**
 * binary_tree_uncle - finds the uncle of a node
 * (sibling of the node's parent)
 * @node: A pointer to the node to find the sibling.
